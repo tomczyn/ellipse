e2fb94ba2330ff9504dd1732b719cba09e76a73d
Maciek <mj.tomczynski@gmail.com>
2021-09-11 14:05:21 +0200

    Added KSP util in buildSrc

--- buildSrc/src/main/java/Util.kt
+++ buildSrc/src/main/java/Util.kt
@@ -36,3 +36,7 @@ fun DependencyHandler.debugImplementation(dependency: String) {
 fun DependencyHandler.kapt(dependency: String) {
     add("kapt", dependency)
 }
+
+fun DependencyHandler.ksp(dependency: String) {
+    add("ksp", dependency)
+}
