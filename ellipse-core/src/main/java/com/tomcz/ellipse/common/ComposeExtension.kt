1c11263b7779c99c071134c53257423cf67ab331
MTomczynski <m.tomczynski@pm.me>
2021-11-23 21:32:42 +0100

    Added collectEffect to compose extensions

--- ellipse-core/src/main/java/com/tomcz/ellipse/common/ComposeExtension.kt
+++ ellipse-core/src/main/java/com/tomcz/ellipse/common/ComposeExtension.kt
@@ -1,6 +1,8 @@
 package com.tomcz.ellipse.common
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
@@ -8,7 +10,13 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import com.tomcz.ellipse.Processor
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.map
 
 @Composable
 fun <EV : Any, ST : Any, T> Processor<EV, ST, *>.collectAsState(
@@ -20,6 +28,19 @@ fun <EV : Any, ST : Any, T> Processor<EV, ST, *>.collectAsState(
     return remember(flow, lifecycleOwner) {
         flow.flowWithLifecycle(lifecycleOwner.lifecycle, lifecycleState)
     }.collectAsState(initial = mapper(state.value))
+}
+
+@SuppressLint("ComposableNaming")
+@Composable
+fun <EV : Any, ST : Any, EF : Any> Processor<EV, ST, EF>.collectEffect(
+    lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
+    collect: suspend (EF) -> Unit
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val flow = remember(effect, lifecycleOwner) {
+        effect.flowWithLifecycle(lifecycleOwner.lifecycle, lifecycleState)
+    }
+    LaunchedEffect(flow) { flow.collect { collect(it) } }
 }
 
 fun <EV : Any, ST : Any, EF : Any> previewProcessor(
